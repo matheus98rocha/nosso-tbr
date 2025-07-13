f19c6bf421545cab652102f2e1ad2e32748cb58a
matheus98rocha <matheus98rocha@gmail.com>
2025-07-13 17:39:19 -0300

    feat: atualizando titulo da página

--- src/modules/bookshelvesBooks/index.tsx
+++ src/modules/bookshelvesBooks/index.tsx
@@ -25,7 +25,7 @@ function BookshelvesBooks() {
 
   return (
     <div className="p-4 space-y-4">
-      <h1 className="text-xl font-bold">Books in Shelf</h1>
+      <h1 className="text-xl font-bold">Livros na estante</h1>
 
       {isPending && <p>Loading...</p>}
       {isError && <p>Error loading books.</p>}
