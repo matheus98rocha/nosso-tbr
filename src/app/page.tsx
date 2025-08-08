464adfafb72413e6b8e91dd045105520fbb4abe7
matheus98rocha <matheus98rocha@gmail.com>
2025-08-08 11:20:08 -0300

    feat: adding suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import { Skeleton } from "@/components/ui/skeleton";
 import ClientHome from "@/modules/home";
+import React, { Suspense } from "react";
 
 export default function HomePage() {
-  return <ClientHome />;
+  return (
+    <Suspense fallback={<Skeleton />}>
+      <ClientHome />
+    </Suspense>
+  );
 }
